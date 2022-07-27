08b1e23a0b297f727960eef406fe2533c09d0453
THEyarik <yaroslavpatcalon@gmail.com>
2022-07-27 12:57:31 +0300

    well done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Posts from "./Components/Posts/posts";
+import ANAKIN_IMAGE from './assets/Anakin.jpg'
 
 function App() {
+    const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
+    const posts = [
+        {
+            id: 1,
+            author: {
+                name: "Anakin Skywalker",
+                photo: ANAKIN_IMAGE,
+                nickname: "@dart_vader",
+            },
+            content: "WTF? Who is Ray? Why she is Skywalker? Luke...?",
+            image: RAY_IMAGE,
+            date: "28 лютого"
+        },
+
+
+
+    ]
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="wrapper">
+            <div className="container">
+                {
+                    posts.map((post) => <Posts data = {post} key={post.id}/> )
+                }
+
+            </div>
+        </div>
+
     </div>
   );
 }
